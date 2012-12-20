8aa6d4be4097ece257b11797b9cbc5767622af5d
Petr <petr.mvd@gmail.com>
2012-12-20 20:39:55 +0100

    Added several semigroup actions, notably `Repeat`.
    `Repeat` is an action of natural numbers on a monoid.

--- Data/Semigroup/Act.hs
+++ Data/Semigroup/Act.hs
@@ -1,6 +1,7 @@
 module Data.Semigroup.Act where
 
 import Data.Semigroup
+import Numeric.Natural.Internal
 
 -- | Represents an action of semigroup @g@ to set @a@.
 --
@@ -23,3 +24,54 @@ instance (SemigroupAct g a, Semigroup g)
 instance (SemigroupAct g a, Monoid g)
   => MonoidAct (Option g) (OptionSet g a) where
 
+
+-- | A wrapper for a group acting on itself.
+newtype SelfAct a = SelfAct a
+  deriving (Show, Read, Eq, Ord)
+instance Functor SelfAct where
+    fmap f (SelfAct x) = SelfAct (f x)
+instance Semigroup g => Semigroup (SelfAct g) where
+    (SelfAct x) <> (SelfAct y) = SelfAct $ x <> y
+instance (Monoid g) => Monoid (SelfAct g) where
+    mempty = SelfAct mempty
+    mappend (SelfAct x) (SelfAct y) = SelfAct $ x `mappend` y
+instance Semigroup g => SemigroupAct (SelfAct g) (SelfAct g) where
+    act = (<>)
+instance (Semigroup g, Monoid g) => MonoidAct (SelfAct g) (SelfAct g) where
+
+-- | A wrapper for an integer acting on an 'Enum'. If the resulting index is
+-- out of the enum bounds, an exception is raised.
+newtype EnumIntAct a = EnumIntAct a
+  deriving (Show, Read, Eq, Ord)
+instance Functor EnumIntAct where
+    fmap f (EnumIntAct x) = EnumIntAct (f x)
+instance (Integral n, Enum a) => SemigroupAct (Sum n) (EnumIntAct a) where
+    act (Sum n) = fmap (toEnum . (+ (fromIntegral n)) . fromEnum)
+
+-- | A wrapper for an integer acting on an instance of both 'Enum' and 'Bounded'.
+-- The index wrap around the bounds, so
+-- @Sum 1 `act` (EnumBoundedIntAct maxBound) == (EnumBoundedIntAct minBound)@ etc.
+newtype EnumBoundedIntAct a = EnumBoundedIntAct a
+  deriving (Show, Read, Eq, Ord)
+instance Functor EnumBoundedIntAct where
+    fmap f (EnumBoundedIntAct x) = EnumBoundedIntAct (f x)
+instance (Bounded a, Enum a, Integral n) => SemigroupAct (Sum n) (EnumBoundedIntAct a) where
+    act (Sum n) = fmap shift
+      where
+        shift x = toEnum $ mn + ((fromEnum x - mn + fromIntegral n) `mod` l)
+          where
+            mn = fromEnum (asTypeOf minBound x)
+            l  = fromEnum (asTypeOf maxBound x) - mn + 1
+
+-- | A wrapper for represeting the action of natural numbers with
+-- multiplication on a monoid.
+newtype Repeat a = Repeat { unwrapRepeat :: a }
+  deriving (Show, Read, Eq, Ord)
+instance Functor Repeat where
+    fmap f (Repeat x) = Repeat (f x)
+-- | The implementation uses 'times1p' which is defined very efficiently
+-- for most semigroups.
+instance (Monoid w, Whole n) => SemigroupAct (Product n) (Repeat w) where
+    act (Product m) (Repeat x)
+        | m == 0    = Repeat mempty
+        | otherwise = Repeat . unwrapMonoid $ times1p (unsafePred m) (WrapMonoid x)
--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-semigroups-actions
-==================
+# semigroups-actions
 
 In mathematics, an action of a semigroup on a set is an operation that
 associates each element of the semigroup is with a transformation on the set.
@@ -7,8 +6,43 @@ See Wikipedia articles on
 [semigroup action](http://en.wikipedia.org/wiki/Semigroup_action) and
 [group action](http://en.wikipedia.org/wiki/Group_action).
 
-Copyright
----------
+## Examples
+
+Similarly to semigroups, semigroup (or monoid) actions arise almost everywhere (if you look for them).
+
+### Natural numbers acting on monoids
+
+The multiplication monoid of natural numbers acts on any other monoid:
+
+    n `act` x = x <> ... <> x -- x appears n-times
+
+So `0 \`act\` x == mempty`, `3 \`act\` x == x <> x <> x` etc. TODO efficiency
+
+This is expressed by `newtype Repeat` and instance
+
+    instance (Monoid w, Whole n) => SemigroupAct (Product n) (Repeat w) where
+
+Many different concepts can be expressed using such an action, including
+
+- repeating a list `n`-times (`replicate`),
+- compose a function `f` `n`-times, commonly denoted as _fⁿ_ in mathematics:
+
+      (Repeat (Endo fⁿ)) = (Product n) `act` (Repeat (Endo f))
+
+## Self-application
+
+Any semigroup (or monoid) can be viewed as acting on itself. In this case, `act` simply becomes `<>`. This is expressed by the `SelfAct` type:
+
+    newtype SelfAct a = SelfAct a
+    instance Semigroup g => Semigroup (SelfAct g) where
+        (SelfAct x) <> (SelfAct y) = SelfAct $ x <> y
+
+## Matrices acting on vectors
+
+Matrices with multiplication can be viewed as a group acting on vectors.
+
+# Copyright
+
 Copyright 2012, Petr Pudlák
 
 Contact: [petr.pudlak.name](http://petr.pudlak.name/) or through github.
